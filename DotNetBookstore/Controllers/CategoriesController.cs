e95aed868f8db992b266dcdd82b63ead1503a0ef
Dario Hesami <dario.hesami@hotmail.com>
2025-10-31 18:16:45 -0400

    Week 8 - P1

--- DotNetBookstore/Controllers/CategoriesController.cs
+++ DotNetBookstore/Controllers/CategoriesController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DotNetBookstore.Data;
 using DotNetBookstore.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DotNetBookstore.Controllers
 {
+    [Authorize]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -25,6 +27,7 @@ namespace DotNetBookstore.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
+        [AllowAnonymous]
         // GET: Categories/Details/5
         public async Task<IActionResult> Details(int? id)
         {
