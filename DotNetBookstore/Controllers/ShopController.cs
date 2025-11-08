bf921ce10ffdb8b53e3dbf3bf5e217766c77c742
Dario Hesami <dario.hesami@hotmail.com>
2025-11-07 19:27:13 -0500

    Week9 - completed

---
+++ DotNetBookstore/Controllers/ShopController.cs
@@ -0,0 +1,21 @@
+﻿using DotNetBookstore.Data;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+
+namespace DotNetBookstore.Controllers
+{
+    public class ShopController : Controller
+    {
+        // Class-level DbContext connection object
+        private readonly ApplicationDbContext _context;
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}
